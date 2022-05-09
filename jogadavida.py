64c2d29e01cdc5acdcaadeae432e892410e188a4
Giovanni Laner <86385629+GiovanniLaner@users.noreply.github.com>
2022-05-09 13:20:02 -0300

    Primeiro código
    Primeiro desenvolvimento do código. Código ainda em desenvolvimento.

---
+++ jogadavida.py
@@ -0,0 +1,58 @@
+
+from random import random
+
+
+jogadorUm = input('Informe o nome do jogador 1:')
+jogadorDois = input('Informe o nome do jogador 2:')
+
+rodarDado = print(random.randint(1, 6)) #sorteia o numero do dado
+
+def executaRegras(numeroJogador, numeroCasa):
+
+    return numeroCasa, numeroJogador
+
+def dadoCasa(numeroJogador):
+
+    return numeroJogador
+
+def morreCasa(numeroJogador):
+    
+    print(numeroJogador)
+
+    return numeroJogador
+
+def desafioMatematicoCasa(numeroJogador):
+
+    return numeroJogador
+
+def formaturaCasa(numeroJogador):
+
+    return numeroJogador
+
+def terFilhosCasa(numeroJogador):
+
+    return numeroJogador
+
+def casarCasa(numeroJogador):
+    
+    return numeroJogador
+
+def ficarFamosoCasa(numeroJogador):
+
+    return numeroJogador
+
+def divorciarCasa(numeroJogador):
+
+    return numeroJogador
+
+def loteriaCasa(numeroJogador):
+    
+    return numeroJogador
+
+def casarCasa(numeroJogador):
+
+    return numeroJogador
+
+def resetarCasas(numeroJogador):
+
+    return numeroJogador
